2e844b0197cb86e0687726936e790f5252ad5da0
Shaun <shaunhohoho@gmail.com>
2022-06-06 13:02:16 +0100

    fizzbuzz solved, rspec added

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -1,7 +1,13 @@
-Hello This is stuff I added
-
-FizzBuzz.rb
-FizzBuzz.rb/FizzBuzz.rb
-/FizzBuzz.rb
-
-cd / change at 12:36 
+def fizzbuzz(number)
+  fb_string = ""
+  if number % 15 == 0
+    fb_string += "fizzbuzz"
+  elsif number % 5 == 0
+    fb_string += "buzz"
+  elsif number % 3 == 0
+    fb_string += "fizz"
+  else
+    fb_string += "#{number}"
+  end
+  return fb_string
+end
