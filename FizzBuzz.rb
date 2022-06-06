5f5a623ff89d10579cc4911f0bd38c240d965436
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-06 12:39:58 +0100

    third commit

--- FizzBuzz.rb
+++ FizzBuzz.rb
@@ -3,10 +3,10 @@ Hello This is stuff I added
 FizzBuzz.rb
 FizzBuzz.rb/FizzBuzz.rb
 /FizzBuzz.rb
-<<<<<<< HEAD
 
 cd / change at 12:36
-=======
 I am changing
 cd /
->>>>>>> 6fe0026163e109e48f086e0d676d9822ba210991
+
+
+I'm changing at 12:39
