072dfce7166be7f97d8c2c9f2d9ab9cff2152040
Tayanne Bencardino <tayannebencardino@gmail.com>
2022-06-06 15:33:35 +0100

    Three tests written

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -6,9 +6,14 @@ describe "fizzbuzz" do
     end
 end
 
-
 describe "fizzbuzz" do
   it 'returns "buzz" when passed 5' do
       expect(fizzbuzz(5)).to eq "buzz"
+  end
+end
+
+describe "fizzbuzz" do
+  it 'return "fizzbuzz when passed 15' do
+    expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
