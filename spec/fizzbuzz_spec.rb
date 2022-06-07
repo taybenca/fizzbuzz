90e98dd00ffff4e8b178611847c4bb747edb9a9d
Tayanne Bencardinogit config --global user.email <tayannebencardino@gmail.com>
2022-06-07 11:24:33 +0100

    Co-authored-by: shaunywho <shaunywho@users.noreply.github.com>

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -13,7 +13,20 @@ describe "fizzbuzz" do
 end
 
 describe "fizzbuzz" do
-  it 'return "fizzbuzz when passed 15' do
+  it 'return "fizzbuzz" when passed 15' do
     expect(fizzbuzz(15)).to eq "fizzbuzz"
   end
 end
+
+describe "fizzbuzz" do
+  it 'return "1" when passed 1' do
+    expect(fizzbuzz(1)).to eq "1"
+  end
+end
+
+describe "fizzbuzz" do
+  it "doesn't return 1 when passed 1" do
+    expect(fizzbuzz(1)).not_to eq 1
+  end
+end
+
